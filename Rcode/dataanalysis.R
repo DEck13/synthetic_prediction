5a891a83dd221bfae1ddb7c48b9f239933f00e0d
Jilei Lin <mac@wirelessprv-10-194-163-252.near.illinois.edu>
2020-08-19 17:40:28 -0500

    edits to typos and unclear points, major edits to Section 3.2 and Section 5

--- Rcode/dataanalysis.R
+++ Rcode/dataanalysis.R
@@ -8,6 +8,9 @@ library('msos')
 library('tikzDevice')
 library('xtable')
 
+# set working directory
+setwd("/Users/mac/Desktop/Research/Post-Shock Prediction/")
+
 ## Conoco Phillips
 getSymbols('COP', from = "2000-01-01")
 COP <- as.data.frame(COP)
@@ -26,8 +29,31 @@ Brent_Crude <- read.csv("https://pkgstore.datahub.io/core/oil-prices/brent-daily
 WTI_Crude <- read.csv("https://pkgstore.datahub.io/core/oil-prices/wti-daily_csv/data/c414c9d375ec3c8f9d7c276d866fb2a4/wti-daily_csv.csv") %>%
   rename(WTI_Close = Price)
 
+## Gold Price
+getSymbols('GC=F', from = "2000-01-01")
+gold <- as.data.frame(`GC=F`)
+gold <- na.omit(gold)
+gold <- gold %>% mutate(Date = rownames(gold))
+
+## Dollar Index
+getSymbols('DX-Y.NYB', from = "2000-01-01")
+USD <- as.data.frame(`DX-Y.NYB`)
+USD <- na.omit(USD)
+USD <- USD %>% mutate(Date = rownames(USD))
+
+## 13-Week T-Bill
+TB <- read.csv('^IRX.csv', na.strings = 'null')
+TB <- na.omit(TB)
+
+## Volatility Index
+getSymbols('^VIX', from = "2000-01-01")
+VIX <- as.data.frame(VIX)
+VIX <- na.omit(VIX)
+VIX <- VIX %>% mutate(Date = rownames(VIX))
+
+
 ## inflation adjustment
-getSymbols("CPIAUCSL", src='FRED')
+getSymbols("CPIAUCSL", src = 'FRED')
 avg.cpi <- apply.yearly(CPIAUCSL, mean)
 inflation_adj <- as.numeric(avg.cpi['2020'])/avg.cpi
 inflation_adj <- as.data.frame(inflation_adj)
@@ -38,21 +64,21 @@ inflation_adj <- inflation_adj %>% mutate(year = 1947:2020)
 # Data Preparation
 COP_close <- COP %>% dplyr::select(COP.Close, Date) %>% rename(COP_Close = COP.Close)
 GSPC_close <- GSPC %>% dplyr::select(GSPC.Close, Date) %>% rename(GSPC_Close = GSPC.Close)
-COP_close <- merge(COP_close, GSPC_close)
-COP_close <- merge(COP_close, WTI_Crude)
+USD_close <- USD %>% dplyr::select(`DX-Y.NYB.Close`, Date) %>% rename(USD_Close = `DX-Y.NYB.Close`)
+TB_close <- TB %>% dplyr::select(Close, Date) %>% rename(TB_Close = Close)
+VIX_close <- VIX %>% dplyr::select(VIX.Close, Date) %>% rename(VIX_Close = VIX.Close)
+
+tom <- list(GSPC_close, WTI_Crude, USD_close, TB_close, VIX_close)
+for (i in 1:length(tom)) {
+  COP_close <- merge(COP_close, tom[[i]])
+}
+
 
 # response
 Y <- COP_close$COP_Close[-1]
 
 # data frame
 COP_close <- data.frame(COP_close[-nrow(COP_close), ], Y)
-
-# lagged covariates
-WTI_Close_lag1 <- COP_close$WTI_Close[-nrow(COP_close)]
-GSPC_Close_lag1 <- COP_close$GSPC_Close[-nrow(COP_close)]
-# re-input
-COP_close <- data.frame(COP_close[-1, ], WTI_Close_lag1, GSPC_Close_lag1)
-
 
 #### Monday, March 17th, 2008
 
@@ -62,16 +88,15 @@ COP_close <- data.frame(COP_close[-1, ], WTI_Close_lag1, GSPC_Close_lag1)
 start <- which(COP_close$Date == "2008-03-14")
 start_day_20080317 <- as.numeric(1:nrow(COP_close) == start)
 COP_close <- COP_close %>% mutate(start_day_20080317 = start_day_20080317)
-TS2 <- COP_close[(start-30):start, ]
+TS2 <- COP_close[(start - 30):start, ]
 # inflation adjustment
-TS2[, 2:7] <- TS2[, 2:7] * inflation_adj$dollars_2020[inflation_adj$year == 2008] 
-m_COP_3_17 <- lm(Y ~ COP_Close + start_day_20080317 + GSPC_Close + WTI_Close, 
+TS2[, 2:8] <- TS2[, 2:8] * inflation_adj$dollars_2020[inflation_adj$year == 2008] 
+m_COP_3_17 <- lm(Y ~ COP_Close + start_day_20080317 + GSPC_Close + WTI_Close + 
+                    USD_Close + TB_Close, 
                  data = TS2)
 alpha_3_17 <- summary(m_COP_3_17)$coef[3,1:2] 
 # shock-effects
 alpha_3_17
-
-
 
 #### 2008 shock effects
 
@@ -87,14 +112,15 @@ COP_close <- COP_close %>% mutate(start_day_09_08_08 = start_day_09_08_08,
                                   start_day_09_12_08 = start_day_09_12_08, 
                                   start_day_09_26_08 = start_day_09_26_08)
 # time window
-TS3 <- COP_close[which(COP_close$Date == "2008-07-28"):which(COP_close$Date == "2008-09-26"), ]
+TS3 <- COP_close[which(COP_close$Date == "2008-08-26"):which(COP_close$Date == "2008-09-26"), ]
 # adjust for inflation
-TS3[, 2:7] <- TS3[, 2:7] * inflation_adj$dollars_2020[inflation_adj$year == 2008] 
+TS3[, 2:8] <- TS3[, 2:8] * inflation_adj$dollars_2020[inflation_adj$year == 2008] 
 # AR(1)
 m_COP_Sept_08 <- lm(Y ~ COP_Close + start_day_09_08_08 + start_day_09_12_08 + 
-                      start_day_09_26_08 + GSPC_Close + WTI_Close, data = TS3)
+                      start_day_09_26_08 + GSPC_Close + WTI_Close + USD_Close + TB_Close, data = TS3)
 alpha_Sept_08 <- summary(m_COP_Sept_08)$coef[3:5,1:2] 
 cov2cor(vcov(m_COP_Sept_08)[3:5, 3:5])
+
 # shock-effects
 alpha_Sept_08
 
@@ -138,14 +164,13 @@ COP_close <- COP_close %>% mutate(start_day_20141127 = start_day_20141127)
 # time window
 TS4 <- COP_close[(start - 30):start,]
 # adjust for inflation
-TS4[, 2:7] <- TS4[, 2:7] * inflation_adj$dollars_2020[inflation_adj$year == 2014] 
+TS4[, 2:8] <- TS4[, 2:8] * inflation_adj$dollars_2020[inflation_adj$year == 2014] 
 # AR(1)
-m_COP_11_27_14 <- lm(Y ~ COP_Close + start_day_20141127 + GSPC_Close + WTI_Close,
+m_COP_11_27_14 <- lm(Y ~ COP_Close + start_day_20141127 + GSPC_Close + WTI_Close + USD_Close + TB_Close, 
                      data = TS4)
 alpha_11_27_14 <- summary(m_COP_11_27_14)$coef[3,1:2] 
 # shock-effects
 alpha_11_27_14
-
 
 
 #### The March 9th, 2020 shock effect:
@@ -158,12 +183,12 @@ COP_close <- COP_close %>% mutate(start_day_20200309 = start_day_20200309)
 TS1 <- COP_close[(start-30):(start), ]
 
 # shock-effect estimate
-m_COP_03_09_20 <- lm(Y ~ COP_Close + start_day_20200309 + GSPC_Close + WTI_Close, 
+m_COP_03_09_20 <- lm(Y ~ COP_Close + start_day_20200309 + GSPC_Close + WTI_Close + 
+                         USD_Close + TB_Close,  
                      data = TS1)
 alpha_03_09_20 <- summary(m_COP_03_09_20)$coef[3,1:2]
 # shock-effects
 alpha_03_09_20
-
 
 
 ## Shock effect estimators
@@ -184,15 +209,22 @@ alpha_IVW <- sum(weights * estimates[, 1])
 Tstar.Date <- c("2020-03-05", "2008-03-13", "2008-09-05", "2008-09-11", "2008-09-25",  "2014-11-25")
 Tstar <- sapply(Tstar.Date, function(x) which(COP_close$Date == x))
 # X1
-X1 <- as.matrix(TS1[nrow(TS1), c(3, 4)])
+X1 <- as.matrix(TS1[nrow(TS1), 3:7])
 # X1 <- as.matrix(COP_close[c(Tstar[1], Tstar[1] + 1), c(3, 4)])
 # X0
 X0 <- c()
 for (i in 1:5) {
- X0[[i]] <- as.matrix(COP_close[Tstar[i + 1] + 1, c(3, 4)])
+ X0[[i]] <- as.matrix(COP_close[Tstar[i + 1] + 1, 3:7])
 }
 
 # SCM
+
+dat <- scale(rbind(X1, do.call('rbind', X0)), center = T, scale = T)
+X1 <- dat[1, , drop = FALSE]
+X0 <- c()
+for (i in 1:5) {
+  X0[[i]] <- dat[i + 1, , drop = FALSE]
+}
 
 # Euclidean metric
 # objective function
@@ -292,7 +324,9 @@ for (b in 1:B) {
       for (t in 1:Ti) {
         datt <- matrix(c(1, yib[t], 
                          ifelse(t == Tstari+ 1, yes = 1, no = 0), 
-                         as.numeric(dat[t, c('GSPC_Close', 'WTI_Close')])))
+                         as.numeric(dat[t, c('GSPC_Close', 'WTI_Close', 
+                                             'USD_Close',
+                                             'TB_Close')])))
         yib <- c(yib, resb[t] + coef %*% datt)
       }
       
@@ -302,12 +336,17 @@ for (b in 1:B) {
                           ifelse(1:Ti == Tstari[1] + 1, yes = 1, no = 0), 
                           ifelse(1:Ti == Tstari[2] + 1, yes = 1, no = 0),
                           ifelse(1:Ti == Tstari[3] + 1, yes = 1, no = 0),
-                          dat[, c('GSPC_Close', 'WTI_Close')])
+                          dat[,  c('GSPC_Close', 'WTI_Close', 
+                                   'USD_Close',
+                                   'TB_Close')])
       # New colnames
-      colnames(datbi) <- c('yblag', 'shock1', 'shock2', 'shock3', c('GSPC_Close', 'WTI_Close'))
+      colnames(datbi) <- c('yblag', 'shock1', 'shock2', 'shock3',  c('GSPC_Close', 'WTI_Close', 
+                                                                     'USD_Close',
+                                                                     'TB_Close'))
       
       # New Linear Model
-      lmodbi <- lm(yb ~ 1 + yblag + shock1 + shock2 + shock3 + GSPC_Close + WTI_Close, dat = datbi)
+      lmodbi <- lm(yb ~ 1 + yblag + shock1 + shock2 + shock3 + GSPC_Close + WTI_Close
+                    + USD_Close + TB_Close, dat = datbi)
       
       # 3 Shock Effects
       alphahatsb <- c(alphahatsb, coef(lmodbi)[3:5])
@@ -326,19 +365,26 @@ for (b in 1:B) {
       
       for (t in 1:Ti) {
         datt <- matrix(c(1, yib[t], ifelse(t == Tstari + 1, yes = 1, no = 0), 
-                         as.numeric(dat[t, c('GSPC_Close', 'WTI_Close')])))
+                         as.numeric(dat[t, c('GSPC_Close', 'WTI_Close', 
+                                             'USD_Close',
+                                             'TB_Close')])))
         yib <- c(yib, resb[t] + coef %*% datt)
       }
       
       # Prepare for new data
       yb <- yib[-1]; yblag <- yib[-(Ti + 1)]
-      datbi <- data.frame(yblag, ifelse(1:Ti == Tstari + 1, yes = 1, no = 0), dat[, c('GSPC_Close', 'WTI_Close')])
+      datbi <- data.frame(yblag, ifelse(1:Ti == Tstari + 1, yes = 1, no = 0), dat[, c('GSPC_Close', 'WTI_Close', 
+                                                                                      'USD_Close',
+                                                                                      'TB_Close')])
       
       # New colnames
-      colnames(datbi) <- c('yblag', 'shock', c('GSPC_Close', 'WTI_Close'))
+      colnames(datbi) <- c('yblag', 'shock', c('GSPC_Close', 'WTI_Close', 
+                                               'USD_Close',
+                                               'TB_Close'))
       
       # New Linear Model
-      lmodbi <- lm(yb ~ 1 + yblag + shock + GSPC_Close + WTI_Close, dat = datbi)
+      lmodbi <- lm(yb ~ 1 + yblag + shock + GSPC_Close + WTI_Close + 
+                       USD_Close + TB_Close, dat = datbi)
       
       # Shock Effects
       alphahatsb <- c(alphahatsb, coef(lmodbi)[3])
@@ -390,10 +436,24 @@ weights <- (1 / estimates[1:4,2]) / sum(1 / estimates[1:4, 2])
 alpha.IVW.additive <- sum(weights * estimates[1:4, 1])
 alpha.IVW.additive <- as.numeric(alpha.IVW.additive + estimates[5, 1])
 
+# X1
+X1 <- as.matrix(TS1[nrow(TS1), 3:7])
+# X1 <- as.matrix(COP_close[c(Tstar[1], Tstar[1] + 1), c(3, 4)])
+# X0
 X0 <- c()
 for (i in 1:4) {
-  X0[[i]] <- as.matrix(COP_close[Tstar[i + 1] + 1, c(3, 4)])
+  X0[[i]] <- as.matrix(COP_close[Tstar[i + 1] + 1, 3:7])
 }
+
+# SCM
+
+dat <- scale(rbind(X1, do.call('rbind', X0)), center = T, scale = T)
+X1 <- dat[1, , drop = FALSE]
+X0 <- c()
+for (i in 1:4) {
+  X0[[i]] <- dat[i + 1, , drop = FALSE]
+}
+
 n <- 4
 # optimization
 outs <- solnp(par = rep(1/n, n), fun = weightedX0, eqfun = Wcons, eqB = 0, LB = rep(0, n), UB = rep(1, n))
@@ -406,14 +466,14 @@ weightedX0(round(outs$pars, digits = 3))
 alpha.wadj.additive <- as.numeric(sum(Wstar.additive * estimates[1:4,1]) + estimates[5, 1])
 
 ## Post-shock forecasts
-m_COP_03_06_20 <- lm(Y ~ COP_Close + GSPC_Close + WTI_Close + GSPC_Close + WTI_Close + 
-                     WTI_Close_lag1 + GSPC_Close_lag1, data = TS1[-nrow(TS1), ])
+m_COP_03_06_20 <- lm(Y ~ COP_Close + GSPC_Close + WTI_Close + USD_Close + TB_Close + VIX_Close, 
+                     data = TS1[-nrow(TS1), ])
 # Yhat 1
-Yhat_nothing <- coef(m_COP_03_06_20) %*% t(as.matrix(cbind(1, as.vector(TS1[nrow(TS1), c(2, 3, 4, 6, 7)]))))
+Yhat_nothing <- coef(m_COP_03_06_20) %*% t(as.matrix(cbind(1, as.vector(TS1[nrow(TS1), 2:7]))))
 # Yhat 2
-Yhat_adj <- c(alpha_adj + Yhat_nothing, alpha_IVW + Yhat_nothing, alpha_wadj + Yhat_nothing)
-Yhat_additive <- c(alpha.adj.additive + Yhat_nothing, alpha.IVW.additive + Yhat_nothing, 
-                   alpha.wadj.additive + Yhat_nothing)
+Yhat_adj <- c(adj = alpha_adj + Yhat_nothing, IVW = alpha_IVW + Yhat_nothing, wadj = alpha_wadj + Yhat_nothing)
+Yhat_additive <- c(adj = alpha.adj.additive + Yhat_nothing, IVW = alpha.IVW.additive + Yhat_nothing, 
+                   wadj = alpha.wadj.additive + Yhat_nothing)
 
 ## doing nothing completely misses the mark
 Yhat_nothing - TS1$Y[nrow(TS1)]
