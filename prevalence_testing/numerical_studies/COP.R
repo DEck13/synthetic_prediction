4c53ddc82386a421a19ce1cc59d285c10e007b80
Jilei <jileil2@Jileis-MacBook-Pro.local>
2022-02-12 17:04:01 -0500

    edits of manuscript and COP example

--- prevalence_testing/numerical_studies/COP.R
+++ prevalence_testing/numerical_studies/COP.R
@@ -186,7 +186,8 @@ ols.est.alphahat <- function(Tstar, Y, X) {
 }
 
 # functions that return alpha.hat and synthetic weights
-ps.indic.W.permanent <- function(Tstar, Y, X, K, H, Ts, ell, B, bw, sig.levl = .05, 
+ps.indic.W.permanent <- function(Tstar, Y, X, K, H, Ts, ell, B, bw, 
+                                 sig.levl = .05, q1, 
                                  retro = TRUE, selfW = NA, scale = FALSE) {
   
   n <- length(Y) - 1
@@ -208,36 +209,40 @@ ps.indic.W.permanent <- function(Tstar, Y, X, K, H, Ts, ell, B, bw, sig.levl = .
       alpha.wadj <- sym$est[2]
     }
     
-    m1.L.i <- matrix(NA, nrow = Ti - Ki - H - 1, ncol = H)
-    m2.L.i <- matrix(NA, nrow = Ti - Ki - H - 1, ncol = H)
+    m1.L.i <- matrix(NA, nrow = Ti - Ki - H - q1, ncol = H)
+    m2.L.i <- matrix(NA, nrow = Ti - Ki - H - q1, ncol = H)
     
     # compute losses
     for (h in 1:H) {
-      for (t in 1:(Ti - Ki - H - 1)) {
+      for (t in 1:(Ti - Ki - H - q1)) {
+        TL <- length(Y[[i]])
+        yi <- Y[[i]][-(1:q1)][(t + H - h + 1):(t + Ki + H - h)]
+        xi <- X[[i]][-(1:q1), ][(t + H - h + 1):(t + Ki + H - h),]
         
-        yi <- Y[[i]][-1][(t + H - h + 1):(t + Ki + H - h)]
-        xi <- X[[i]][-1, ][(t + H - h + 1):(t + Ki + H - h),]
-        
-        # lag
-        yilag <- Y[[i]][-(Ti + 1)][(t + H - h + 1):(t + Ki + H - h)]
+        # yi lags
+        yilags <- c()
+        for (j in 1:q1) {
+          yijlag <- lag(Y[[i]][-(1:q1)], n = j)[(t + H - h + 1):(t + Ki + H - h)]
+          yilags <- cbind(yilags, yijlag)
+        }
         
         # OLS
         lmodi.adj <- lm(yi ~ 1 + ifelse((t + H - h + 1):(t + Ki + H - h) >= Tstari + 1, yes = 1, no = 0) + 
-                          yilag + xi)
-        lmodi.unadj <- lm(yi ~ 1 + yilag + xi)
+                          yilags + xi)
+        lmodi.unadj <- lm(yi ~ 1 + yilags + xi)
         
         # beta.hat
         beta.hat.adj <- matrix(coef(lmodi.adj), nrow = 1)
         beta.hat.adj[which(is.na(beta.hat.adj) == TRUE)] <- 0
         beta.hat.unadj <- matrix(coef(lmodi.unadj), nrow = 1)
         
-        yhat.adj <- tail(yi, 1)
-        yhat.unadj <- tail(yi, 1)
+        yhat.adj <- Y[[i]][(t + Ki + H - h + 1 - q1):(t + Ki + H - h)]
+        yhat.unadj <- Y[[i]][(t + Ki + H - h + 1 - q1):(t + Ki + H - h)]
         
         for (j in 1:h) {
           yhat.adj.h <- matrix(c(1, ifelse(t + Ki + H - h + j >= Tstari + 1, yes = 1, no = 0),
-                                 yhat.adj[j], X[[i]][-1, ][t + Ki + H - h + j, ]))
-          yhat.unadj.h <- beta.hat.unadj %*% matrix(c(1, yhat.unadj[j], X[[i]][-1, ][t + Ki + H - h + j, ]))
+                                 yhat.adj[j:(j + q1 - 1)], X[[i]][-(1:q1), ][t + Ki + H - h + j, ]))
+          yhat.unadj.h <- beta.hat.unadj %*% matrix(c(1, yhat.adj[j:(j + q1 - 1)], X[[i]][-(1:q1), ][t + Ki + H - h + j, ]))
           
           if (t + Ki + H - h == Tstari & i >= 3 & h == 1) {
             yhat.adj.h <- yhat.adj.h + alpha.wadj
@@ -248,8 +253,8 @@ ps.indic.W.permanent <- function(Tstar, Y, X, K, H, Ts, ell, B, bw, sig.levl = .
         }
         
         # losses
-        m1.L.i[t, h] <- sel(y = Y[[i]][-1][t + Ki + H], yhat = yhat.adj[h + 1])
-        m2.L.i[t, h] <- sel(y = Y[[i]][-1][t + Ki + H], yhat = yhat.unadj[h + 1])
+        m1.L.i[t, h] <- sel(y = Y[[i]][-1][t + Ki + H], yhat = yhat.adj[h + q1])
+        m2.L.i[t, h] <- sel(y = Y[[i]][-1][t + Ki + H], yhat = yhat.unadj[h + q1])
       }
     }
     # loss differential
@@ -316,7 +321,7 @@ ps.indic.W.dynamic <- function(Tstar, Y, X, K, H, Ts,
         # yi lags
         yilags <- c()
         for (j in 1:q1) {
-          yijlag <- Y[[i]][-c(1:(q1 - j), (TL - (q1 - 1)):(TL - (q1 - j)))][(t + H - h + 1):(t + Ki + H - h)]
+          yijlag <- lag(Y[[i]][-(1:max(q1, q2 - 1))], n = j)[(t + H - h + 1):(t + Ki + H - h)]
           yilags <- cbind(yilags, yijlag)
         }
         
@@ -325,7 +330,7 @@ ps.indic.W.dynamic <- function(Tstar, Y, X, K, H, Ts,
         if (q2 > 1) {
           
           for (j in 1:(q2 - 1)) {
-            xj.xlags <- X[[i]][-c(1:(q2 - 1 - j), (TL - (q2 - 1 - 1)):(TL - (q2 - 1 - j))), ][(t + H - h + 1):(t + Ki + H - h), ]
+            xj.xlags <- lag(X[[i]][-(1:max(q1, q2 - 1)), ], n = j)[(t + H - h + 1):(t + Ki + H - h), ]
             x.xlags <- cbind(x.xlags, xj.xlags)
           }
         }
@@ -372,7 +377,6 @@ ps.indic.W.dynamic <- function(Tstar, Y, X, K, H, Ts,
             
             x.xlags.for.pred <- X[[i]][-(1:q1), ][t + Ki + H - h + j, ]
             if (q2 > 1) {
-              
               for (k in 1:(q2 - 1)) {
                 x.xlags.for.pred <- cbind(x.xlags.for.pred, 
                                           X[[i]][-1, ][t + Ki + H - h + j - k, ])
@@ -421,7 +425,7 @@ ps.indic.W.dynamic <- function(Tstar, Y, X, K, H, Ts,
               
               for (k in 1:(q2 - 1)) {
                 x.xlags.for.pred <- cbind(x.xlags.for.pred, 
-                                          X[[i]][-1, ][t + Ki + H - h + j - k, ])
+                                          X[[i]][-(1:q1), ][t + Ki + H - h + j - k, ])
               }
             }
             D.i.t <- ifelse(t + Ki + H - h + j >= Tstari + 1, yes = 1, no = 0) 
@@ -530,7 +534,7 @@ avg.cpi <- apply.yearly(CPIAUCSL, mean)
 inflation_adj <- as.numeric(avg.cpi['2020'])/avg.cpi
 inflation_adj <- as.data.frame(inflation_adj)
 colnames(inflation_adj) <- c("dollars_2020")
-inflation_adj <- inflation_adj %>% mutate(year = 1947:2021)
+inflation_adj <- inflation_adj %>% mutate(year = 1947:2022)
 
 
 # Data Preparation
@@ -562,7 +566,7 @@ H <- 10
 # training sample size
 K <- 30
 # number of days after shock date
-L <- 30
+L <- 7
 
 #### Monday, March 17th, 2008
 
@@ -577,11 +581,12 @@ TS2 <- COP_close[(start - (1 + 15 + K + H)):(start + L),]
 TS2[, 2:8] <- TS2[, 2:8] * inflation_adj$dollars_2020[inflation_adj$year == 2008] 
 
 
-m_COP_3_17.null <- lm(Y ~ COP_Close + GSPC_Close + WTI_Close + 
-                   USD_Close + TB_Close, 
-                 data = TS2)
-m_COP_3_17 <- lm(Y ~ COP_Close + start_day_20080317 + GSPC_Close + WTI_Close + 
-                   USD_Close + TB_Close, 
+m_COP_3_17.null <- lm(Y ~ COP_Close + ylag2 + GSPC_Close + WTI_Close + USD_Close + TB_Close + VIX_Close + 
+                        GSPC_Close.lag + WTI_Close.lag + USD_Close.lag + TB_Close.lag + VIX_Close.lag, 
+                      data = TS2)
+m_COP_3_17 <- lm(Y ~ COP_Close + ylag2 + GSPC_Close + WTI_Close + USD_Close + TB_Close + VIX_Close + 
+                   GSPC_Close.lag + WTI_Close.lag + USD_Close.lag + TB_Close.lag + VIX_Close.lag + 
+                   start_day_20080317, 
                  data = TS2)
 
 # dynamic
@@ -612,11 +617,14 @@ TS3 <- COP_close[(start - (1 + 15 + K + H)):(start + L),]
 TS3[, 2:8] <- TS3[, 2:8] * inflation_adj$dollars_2020[inflation_adj$year == 2008] 
 
 start2 <- which(TS3$Date == "2008-09-08")
-expterm <- exp(-(1:nrow(TS3) - start2)) * ifelse(1:nrow(TS3) >= start2, yes = 1, no = 0)
 
 # models
-m_COP_null <- lm(Y ~ COP_Close + GSPC_Close + WTI_Close + USD_Close + TB_Close, data = TS3)
-m_COP_Sept_08 <- lm(Y ~ COP_Close + start_day_20080908 + GSPC_Close + WTI_Close + USD_Close + TB_Close, data = TS3)
+m_COP_null <- lm(Y ~ COP_Close + ylag2 + GSPC_Close + WTI_Close + USD_Close + TB_Close + VIX_Close + 
+                   GSPC_Close.lag + WTI_Close.lag + USD_Close.lag + TB_Close.lag + VIX_Close.lag, 
+                 data = TS3)
+m_COP_Sept_08 <- lm(Y ~ COP_Close + ylag2 + GSPC_Close + WTI_Close + USD_Close + TB_Close + VIX_Close + 
+                      GSPC_Close.lag + WTI_Close.lag + USD_Close.lag + TB_Close.lag + VIX_Close.lag +
+                      start_day_20080908, data = TS3)
 # dynamic
 co.D <- c()
 for (d in 1:nrow(TS3)) {
@@ -625,7 +633,6 @@ for (d in 1:nrow(TS3)) {
 co.D <- as.matrix(co.D)
 
 m_COP.dynamic <- lm(TS3$Y ~ as.matrix(TS3[, c(2:7, 9:14)]) + co.D + TS3$start_day_20080908)
-
 
 # AICs
 AIC.20080908.null <- AIC(m_COP_null) 
@@ -648,8 +655,10 @@ TS4 <- COP_close[(start - (1 + 15 + K + H)):(start + L),]
 TS4[, 2:8] <- TS4[, 2:8] * inflation_adj$dollars_2020[inflation_adj$year == 2014] 
 
 # models
-m_COP_11_27_14.null <- lm(Y ~ COP_Close + GSPC_Close + WTI_Close + USD_Close + TB_Close, data = TS4)
-m_COP_11_27_14 <- lm(Y ~ COP_Close + start_day_20141127 + GSPC_Close + WTI_Close + USD_Close + TB_Close, 
+m_COP_11_27_14.null <- lm(Y ~ COP_Close + ylag2 + GSPC_Close + WTI_Close + USD_Close + TB_Close + VIX_Close + 
+                            GSPC_Close.lag + WTI_Close.lag + USD_Close.lag + TB_Close.lag + VIX_Close.lag, data = TS4)
+m_COP_11_27_14 <- lm(Y ~ COP_Close + ylag2 + GSPC_Close + WTI_Close + USD_Close + TB_Close + VIX_Close + 
+                       GSPC_Close.lag + WTI_Close.lag + USD_Close.lag + TB_Close.lag + VIX_Close.lag + start_day_20141127, 
                      data = TS4)
 # dynamic
 co.D <- c()
@@ -660,14 +669,12 @@ co.D <- as.matrix(co.D)
 
 m_COP.dynamic <- lm(TS4$Y ~ as.matrix(TS4[, c(2:7, 9:14)]) + co.D + TS4$start_day_20141127)
 
-
 # AICs of different models
 AIC.20141127.null <- AIC(m_COP_11_27_14.null) 
 AIC.20141127.permanent <- AIC(m_COP_11_27_14) 
 AIC.20141127.dynamic <- AIC(m_COP.dynamic) 
 
 AIC.20141127 <- c(AIC.20141127.null, AIC.20141127.permanent, 
-                  AIC.20141127.decay1, AIC.20141127.decay2,
                   AIC.20141127.dynamic)
 
 
@@ -680,11 +687,11 @@ COP_close <- COP_close %>% mutate(start_day_20200309 = start_day_20200309)
 # time window
 TS1 <- COP_close[(start - (1 + 15 + K + H)):(start + L),]
 
-m_COP_03_09_20.null <- lm(Y ~ COP_Close + GSPC_Close + WTI_Close + 
-                       USD_Close + TB_Close,  
+m_COP_03_09_20.null <- lm(Y ~ COP_Close + ylag2 + GSPC_Close + WTI_Close + USD_Close + TB_Close + VIX_Close + 
+                            GSPC_Close.lag + WTI_Close.lag + USD_Close.lag + TB_Close.lag + VIX_Close.lag,  
                      data = TS1)
-m_COP_03_09_20 <- lm(Y ~ COP_Close + start_day_20200309 + GSPC_Close + WTI_Close + 
-                       USD_Close + TB_Close,  
+m_COP_03_09_20 <- lm(Y ~ COP_Close + ylag2 + GSPC_Close + WTI_Close + USD_Close + TB_Close + VIX_Close + 
+                       GSPC_Close.lag + WTI_Close.lag + USD_Close.lag + TB_Close.lag + VIX_Close.lag + start_day_20200309,  
                      data = TS1)
 # dynamic
 co.D <- c()
@@ -694,7 +701,6 @@ for (d in 1:nrow(TS4)) {
 co.D <- as.matrix(co.D)
 
 m_COP.dynamic <- lm(TS1$Y ~ as.matrix(TS1[, c(2:7, 9:14)]) + co.D + TS1$start_day_20200309)
-
 
 AIC.20200309.null <- AIC(m_COP_03_09_20.null)
 AIC.20200309.permanent <- AIC(m_COP_03_09_20)
@@ -710,17 +716,18 @@ Ts <- c(nrow(TS1), nrow(TS2), nrow(TS3), nrow(TS4))
 Tstar <- c(which(TS1$Date == "2020-03-05"),
            which(TS2$Date == "2008-03-13"),
            which(TS3$Date == "2008-09-05"), 
-           which(TS6$Date == "2014-11-25"))
+           which(TS4$Date == "2014-11-25"))
 # Y
 Y <- c()
 X <- c()
 TS <- list(TS1, TS2, TS3, TS4)
 for (i in 1:4) {
   Y[[i]] <- TS[[i]]$Y
-  X[[i]] <- as.matrix(TS[[i]][, 3:7])
+  X[[i]] <- as.matrix(TS[[i]][, c(3:7, 10:14)])
 }
 
 res1 <- ps.indic.W.permanent(Tstar = Tstar, Y = Y, X = X, K = rep(K, 4), H = H,
+                             q1 = 2, 
                              Ts = Ts, ell = 4, B = 200, bw = 4, scale = TRUE)
 res2 <- ps.indic.W.dynamic(Tstar = Tstar, Y = Y, X = X, K = rep(K, 4), 
                               q1 = 2, q2 = 2, 
